a256b69a4f36700f17a2d7fef9e1b9f5adcad8e8
mauroslucios <mauroslucios@gmail.com>
2022-01-05 20:33:23 -0300

    Gravando e lendo bytes de um arquivo

--- src/main/java/one/digitalinnovation/stream/bytestream/ExemploIOData.java
+++ src/main/java/one/digitalinnovation/stream/bytestream/ExemploIOData.java
@@ -31,8 +31,7 @@ public class ExemploIOData {
         double preco = scan.nextDouble();
         dos.writeDouble(preco);
 
-        ps.printf("O arquivo %s foi criado com %d bytes no diretório '%s'\n",f.getName(),f.length()
-        , f.getAbsolutePath());
+        ps.printf("O arquivo %s foi criado com %d bytes no diretório '%s'\n",f.getName(),f.length(), f.getAbsolutePath());
 
         lerProduto(f.getPath());
 
@@ -45,7 +44,7 @@ public class ExemploIOData {
         File f = new File(caminhoArquivo);
 //      InputStream is = new FileInputStream(f.getPath());
 //      DataInputStream dis = new DataInputStream();
-        DataInputStream dis = new DataInputStream(FileInputStream(f.getPath()));
+        DataInputStream dis = new DataInputStream(new FileInputStream(f.getPath()));
 
         String nome = dis.readUTF();
         char tamanho = dis.readChar();
@@ -56,7 +55,7 @@ public class ExemploIOData {
         System.out.printf("\nTamanho..............:%s\n", tamanho);
         System.out.printf("\nQuantidade...........:%s\n", quantidade);
         System.out.printf("\nPreço................:%s\n", preco);
-        System.out.print("Total do valor das peças: "+ (quantidade * preco))
+        System.out.print("Total do valor das peças: "+ (quantidade * preco)+"\n");
         dis.close();
     }
 
